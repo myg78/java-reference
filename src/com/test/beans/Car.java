6bedd745901876fcf37980628ca8a626a464181f
mykz <mykz@10.0.0.171>
2018-05-26 17:25:02 -0400

    added car and updated test

---
+++ src/com/test/beans/Car.java
@@ -0,0 +1,28 @@
+package com.test.beans;
+
+public class Car {
+	
+	private String year;
+	private String make;
+	private String model;
+	
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+}
